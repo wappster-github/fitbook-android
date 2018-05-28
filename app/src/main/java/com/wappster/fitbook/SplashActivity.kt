440e1af7d8ccf03846d2e3da0f09a5d47521efb2
haroun.smida <smida.haroun@gmail.com>
2018-05-28 10:49:46 +0100

    Create Project

---
+++ app/src/main/java/com/wappster/fitbook/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.wappster.fitbook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
